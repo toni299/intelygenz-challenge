0ef8c79bc13f940345a3ab1da6b5577aeed0f9ea
Antoni Felix <antoni.felixdeltell@dxc.com>
2024-06-16 10:04:39 +0200

    feature: add port and host parameters to http server

--- challenge-1/main.py
+++ challenge-1/main.py
@@ -1,12 +1,21 @@
-import http.server
-import socketserver
-from http import HTTPStatus
+import argparse
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class Handler(http.server.SimpleHTTPRequestHandler):
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b'Hello world!')
+        self.wfile.write(b"Hello World!")
 
-httpd = socketserver.TCPServer(('', 8000), Handler)
-httpd.serve_forever()
+def main():
+    parser = argparse.ArgumentParser(description='HTTP Server arguments')
+    parser.add_argument('--port', type=int, default=8080, help='Port')
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='Host')
+    args = parser.parse_args()
+    server_address = (args.host, args.port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
