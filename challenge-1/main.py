6ff613bfdc424bdd7dea27d883b916dbd9db9b55
Antoni Felix <antoni.felixdeltell@dxc.com>
2024-06-16 11:57:58 +0200

    feature: add helm chart and fix challenge 1 image and code

--- challenge-1/main.py
+++ challenge-1/main.py
@@ -10,11 +10,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
 def main():
     parser = argparse.ArgumentParser(description='HTTP Server arguments')
-    parser.add_argument('--port', type=int, default=8080, help='Port')
+    parser.add_argument('--port', type=int, default=8000, help='Port')
     parser.add_argument('--host', type=str, default='0.0.0.0', help='Host')
     args = parser.parse_args()
     server_address = (args.host, args.port)
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print(f"Starting server at {args.host}:{args.port}")
     httpd.serve_forever()
 
 if __name__ == "__main__":
